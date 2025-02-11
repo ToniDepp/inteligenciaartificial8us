27ae0916111e6b4d1d7c32f0c9566a0b88baf57e
ToniDepp <124863328+ToniDepp@users.noreply.github.com>
2025-02-11 08:30:05 -0600

    Ejercicio 2 :)

---
+++ Ejercicios/E2CLASE.py
@@ -0,0 +1,13 @@
+#Declarar variables
+nombre = "Juan"
+edad = 25
+altura = 1.75
+
+#Imprimir informacion
+print("Nombre: ",nombre)
+print("Edad: ",edad)
+print("Altura: ",altura)
+
+#Operacion simple
+edad_en_5_anos = edad + 5
+print("En 5 años, tendra ",edad_en_5_anos," años")
